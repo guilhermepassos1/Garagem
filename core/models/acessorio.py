32d062fba52f0ec7c91476afdaaed128eb27ac3d
Guilherme Passos <gepassos06@gmail.com>
2024-04-08 15:02:32 -0300

    criação da model acessorio

--- core/models/acessorio.py
+++ core/models/acessorio.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-class Categoria(models.Model):
+class Acessorio(models.Model):
     descricao = models.CharField(max_length=100)
     def __str__(self):
         return self.descricao
-    
-
